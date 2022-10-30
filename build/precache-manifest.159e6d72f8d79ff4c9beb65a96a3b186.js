2c1093f0f8df65d5f0d533de44e38df76f79f87f
abdelaziz mrabet <mrabetabdelaziz@gmail.com>
2022-10-30 13:49:21 +0100

    deploying

---
+++ build/precache-manifest.159e6d72f8d79ff4c9beb65a96a3b186.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d007633d28a3d4f73aaa08f135789ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cbd2903db5b58c425c5",
+    "url": "/static/css/main.56c9d2d6.chunk.css"
+  },
+  {
+    "revision": "b639d45c0f8dea7ef07c",
+    "url": "/static/js/2.01760f6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01760f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbd2903db5b58c425c5",
+    "url": "/static/js/main.6eefcb38.chunk.js"
+  },
+  {
+    "revision": "fe27408660b659d06efd",
+    "url": "/static/js/runtime-main.ba329fee.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
